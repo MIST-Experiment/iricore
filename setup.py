d6e45cd30a90203c599e1ee1bd91bfcda0dba3d2
lap1dem <vadym.bidula@gmail.com>
2022-04-29 16:08:52 -0400

    setup fix

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ if __name__ == "__main__":
           package_dir={"": "src"},
           packages=find_packages(where="src"),
           ext_modules=[iri_fcore],
+          package_data={'': ['*.asc', 'src/iricore/data/ccir/*.asc']},
+          include_package_data=True,
           python_requires=">=3.7, <3.10",
           classifiers=[
               # How mature is this project? Common values are
