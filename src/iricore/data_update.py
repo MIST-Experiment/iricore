5e6e45ca5dedd9dd5d430de90cfae4c288296d61
lap1dem <vadym.bidula@gmail.com>
2022-05-24 00:33:21 -0400

    Daily files update added

--- src/iricore/data_update.py
+++ src/iricore/data_update.py
@@ -3,8 +3,14 @@ def update():
     import os
     location = os.path.dirname(os.path.abspath(__file__))
     try:
-        request.urlretrieve('http://irimodel.org/indices/apf107.dat', os.path.join(location, 'data/index/apf107.dat'))
-        request.urlretrieve('http://irimodel.org/indices/ig_rz.dat', os.path.join(location, 'data/index/ig_rz.dat'))
+        request.urlretrieve(
+            'https://chain-new.chain-project.net/echaim_downloads/apf107.dat',
+            os.path.join(location, 'data/index/apf107.dat')
+        )
+        request.urlretrieve(
+            'https://chain-new.chain-project.net/echaim_downloads/ig_rz.dat',
+            os.path.join(location, 'data/index/ig_rz.dat')
+        )
         print('The index data was successfully updated!')
     except error.URLError:
         print('Something went wrong. Check internet connection.')
