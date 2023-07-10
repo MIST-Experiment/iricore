0acb7720ceeff331f23287bcb2f1409937d1be79
lap1dem <vadym.bidula@gmail.com>
2023-07-10 12:12:14 -0400

    package compilation fixed

---
+++ build_old.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict
+
+from setuptools_cpp import CMakeExtension, ExtensionBuilder
+# from cmake_build_extension import CMakeExtension, BuildExtension
+
+ext_modules = [
+    CMakeExtension("iricore", sourcedir="src/iricore"),
+]
+
+
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmdclass": dict(build_ext=ExtensionBuilder),
+            "zip_safe": False,
+        }
+    )
