73818e25fc3473705e65a486f242e6ea6cc58977
vbidula <vadym.bidula@gmail.com>
2024-10-21 11:10:34 +0200

    Fixed documentation version number

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ author = 'Vadym Bidula'
 email = 'vadym.bidula@gmail.com'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.8.3'
 
 # -- General configuration ---------------------------------------------------
 
